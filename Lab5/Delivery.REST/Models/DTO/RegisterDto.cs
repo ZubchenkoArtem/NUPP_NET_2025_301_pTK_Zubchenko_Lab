170a165a9eec7b22ed74ec5fcec1e248bc70d25f
ZubchenkoArtem <144624260+ZubchenkoArtem@users.noreply.github.com>
2025-11-22 01:05:04 +0200

    Revert "Lab5.1"

--- Lab5/Delivery.REST/Models/DTO/RegisterDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Delivery.REST.Models.DTO
-{
-    public class RegisterDto
-    {
-        public required string Email { get; set; }
-        public required string Password { get; set; }
-        public required string FullName { get; set; }
-    }
-}
